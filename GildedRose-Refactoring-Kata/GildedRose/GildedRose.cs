ced2eef9c9e162432bd2b18118ef609b250de81e
Harman Khangura <harman198@gmail.com>
2025-06-09 01:03:11 +1000

    Refactored Sulfuras and Default item quality update methods

--- GildedRose-Refactoring-Kata/GildedRose/GildedRose.cs
+++ GildedRose-Refactoring-Kata/GildedRose/GildedRose.cs
@@ -19,36 +19,37 @@ public class GildedRose(IList<Item> Items)
             {
                 UpdateBackstagePassesItemQuality(item);
             }
+            else if (item.Name == "Sulfuras, Hand of Ragnaros")
+            {
+                UpdateSulfurasItemQuality(item);
+            }
             else
             {
-                if (item.Quality > 0)
-                {
-                    if (item.Name != "Sulfuras, Hand of Ragnaros")
-                    {
-                        item.Quality = item.Quality - 1;
-                    }
-                }
-
-                if (item.Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    item.SellIn = item.SellIn - 1;
-                }
-
-                if (item.SellIn < 0)
-                {
-                    if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (item.Quality > 0)
-                        {
-                            if (item.Name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                item.Quality = item.Quality - 1;
-                            }
-                        }
-                    }
-                }
+                UpdateDefaultItemQuality(item);
             }
         }
+    }
+
+    private static void UpdateDefaultItemQuality(Item item)
+    {
+        if (item.Quality > 0)
+        {
+            item.Quality = item.Quality - 1;
+        }
+
+        item.SellIn = item.SellIn - 1;
+
+        if (item.SellIn < 0)
+        {
+            if (item.Quality > 0)
+            {
+                item.Quality = item.Quality - 1;
+            }
+        }
+    }
+
+    private static void UpdateSulfurasItemQuality(Item item)
+    {
     }
 
     private static void UpdateBackstagePassesItemQuality(Item item)
