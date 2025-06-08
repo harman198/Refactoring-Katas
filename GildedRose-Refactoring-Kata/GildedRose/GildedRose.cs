826e7582153cef23fc21ac678e7b940f609fe4de
Harman Khangura <harman198@gmail.com>
2025-06-09 00:56:28 +1000

    Refactored and extracted method for Backstage passes quality

--- GildedRose-Refactoring-Kata/GildedRose/GildedRose.cs
+++ GildedRose-Refactoring-Kata/GildedRose/GildedRose.cs
@@ -14,60 +14,12 @@ public class GildedRose(IList<Item> Items)
             {
                 UpdateAgedBrieItemQuality(item);
             }
+            else if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+
+            {
+                UpdateBackstagePassesItemQuality(item);
+            }
             else
-            {
-                UpdateOtherItemQuality(item);
-            }
-        }
-    }
-
-    private static void UpdateOtherItemQuality(Item item)
-    {
-        if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
-        {
-            if (item.Quality > 0)
-            {
-                if (item.Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    item.Quality = item.Quality - 1;
-                }
-            }
-        }
-        else
-        {
-            if (item.Quality < 50)
-            {
-                item.Quality = item.Quality + 1;
-
-                if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (item.SellIn < 11)
-                    {
-                        if (item.Quality < 50)
-                        {
-                            item.Quality = item.Quality + 1;
-                        }
-                    }
-
-                    if (item.SellIn < 6)
-                    {
-                        if (item.Quality < 50)
-                        {
-                            item.Quality = item.Quality + 1;
-                        }
-                    }
-                }
-            }
-        }
-
-        if (item.Name != "Sulfuras, Hand of Ragnaros")
-        {
-            item.SellIn = item.SellIn - 1;
-        }
-
-        if (item.SellIn < 0)
-        {
-            if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
             {
                 if (item.Quality > 0)
                 {
@@ -76,11 +28,57 @@ public class GildedRose(IList<Item> Items)
                         item.Quality = item.Quality - 1;
                     }
                 }
+
+                if (item.Name != "Sulfuras, Hand of Ragnaros")
+                {
+                    item.SellIn = item.SellIn - 1;
+                }
+
+                if (item.SellIn < 0)
+                {
+                    if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        if (item.Quality > 0)
+                        {
+                            if (item.Name != "Sulfuras, Hand of Ragnaros")
+                            {
+                                item.Quality = item.Quality - 1;
+                            }
+                        }
+                    }
+                }
             }
-            else
+        }
+    }
+
+    private static void UpdateBackstagePassesItemQuality(Item item)
+    {
+        if (item.Quality < 50)
+        {
+            item.Quality = item.Quality + 1;
+
+            if (item.SellIn < 11)
             {
-                item.Quality = item.Quality - item.Quality;
+                if (item.Quality < 50)
+                {
+                    item.Quality = item.Quality + 1;
+                }
             }
+
+            if (item.SellIn < 6)
+            {
+                if (item.Quality < 50)
+                {
+                    item.Quality = item.Quality + 1;
+                }
+            }
+        }
+
+        item.SellIn = item.SellIn - 1;
+
+        if (item.SellIn < 0)
+        {
+            item.Quality = item.Quality - item.Quality;
         }
     }
 
